9a50e84702ced0d4c3d0473df5e5897cb5430ca0
josefilho <iamjose.filho@gmail.com>
2021-04-14 01:07:47 -0400

    first commit

---
+++ cmake-build-debug/CMakeFiles/cpu_6502.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpu_6502.dir/main.cpp.o"
+  "cpu_6502"
+  "cpu_6502.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpu_6502.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cstdlib>
+
+struct CPU {
+  using Byte = unsigned char;
+  using Word = unsigned short;
+
+  Word PC;
+
+};
+
+
+int main() {
+  return 0;
+}
